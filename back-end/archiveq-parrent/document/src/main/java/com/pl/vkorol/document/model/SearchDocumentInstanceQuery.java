9afbb8e27ed3d13a58b01042e4df669e0a10169b
Vasyl Korol mac <gusak.guska223@gmail.com>
2024-06-09 19:58:42 +0200

    modified crating of search document instance query

--- back-end/archiveq-parrent/document/src/main/java/com/pl/vkorol/document/model/SearchDocumentInstanceQuery.java
+++ back-end/archiveq-parrent/document/src/main/java/com/pl/vkorol/document/model/SearchDocumentInstanceQuery.java
@@ -22,40 +22,51 @@ public class SearchDocumentInstanceQuery {
         return new SearchDocumentInstanceQuery(documentDto.getDocumentName(), documentDto.getDescriptors());
     }
 
-        public String generateJpqlQuery() {
-            StringBuilder jpql = new StringBuilder("SELECT d FROM document_instance d");
-            boolean hasConditions = false;
+    public String generateJpqlQuery() {
+        StringBuilder jpql = new StringBuilder("SELECT d FROM document_instance d");
+        boolean hasConditions = false;
 
-            if (documentName != null && !documentName.isEmpty()) {
-                jpql.append(" WHERE d.template.documentName = :documentName");
-                hasConditions = true;
-            }
+        if (documentName != null && !documentName.isEmpty()) {
+            jpql.append(" WHERE d.template.documentName = :documentName");
+            hasConditions = true;
+        }
 
-            if (descriptorDtoList != null && !descriptorDtoList.isEmpty()) {
-                for (int i = 0; i < descriptorDtoList.size(); i++) {
+        if (descriptorDtoList != null && !descriptorDtoList.isEmpty()) {
+            for (int i = 0; i < descriptorDtoList.size(); i++) {
+                String descriptorValue = descriptorDtoList.get(i).value();
+                if (descriptorValue != null && !descriptorValue.isEmpty()) {
                     if (hasConditions) {
                         jpql.append(" AND");
                     } else {
                         jpql.append(" WHERE");
                         hasConditions = true;
                     }
-                    jpql.append(" EXISTS (SELECT 1 FROM d.descriptors desc WHERE desc.archiveDescriptor.name = :name").append(i).append(" AND desc.value = :value").append(i).append(")");
+                    jpql
+                            .append(" EXISTS (SELECT 1 FROM d.descriptors desc WHERE desc.archiveDescriptor.name = :name")
+                            .append(i)
+                            .append(" AND desc.value = :value")
+                            .append(i)
+                            .append(")");
                 }
             }
-
-            return jpql.toString();
         }
 
-        public void setQueryParameters(Query query) {
-            if (documentName != null && !documentName.isEmpty()) {
-                query.setParameter("documentName", documentName);
+        return jpql.toString();
+    }
+
+    public void setQueryParameters(Query query) {
+        if (documentName != null && !documentName.isEmpty()) {
+            query.setParameter("documentName", documentName);
+        }
+        if (descriptorDtoList != null && !descriptorDtoList.isEmpty()) {
+            for (int i = 0; i < descriptorDtoList.size(); i++) {
+                String descriptorValue = descriptorDtoList.get(i).value();
+                if (descriptorValue != null && !descriptorValue.isEmpty()) {
+                DescriptorDto descriptorDto = descriptorDtoList.get(i);
+                query.setParameter("name" + i, descriptorDto.name());
+                query.setParameter("value" + i, descriptorDto.value());
             }
-            if (descriptorDtoList != null && !descriptorDtoList.isEmpty()) {
-                for (int i = 0; i < descriptorDtoList.size(); i++) {
-                    DescriptorDto descriptorDto = descriptorDtoList.get(i);
-                    query.setParameter("name" + i, descriptorDto.name());
-                    query.setParameter("value" + i, descriptorDto.value());
-                }
             }
         }
     }
+}
--- back-end/archiveq-parrent/document/src/main/java/com/pl/vkorol/document/model/payload/DescriptorDto.java
+++ back-end/archiveq-parrent/document/src/main/java/com/pl/vkorol/document/model/payload/DescriptorDto.java
@@ -1,8 +1,11 @@
 package com.pl.vkorol.document.model.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record DescriptorDto(
+
         @JsonProperty("name")
         String name,
         @JsonProperty("value")
